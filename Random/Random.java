6161052a1dbf3fbea34daf51c0e5fd5b11a899f6
Duclings <155932195+Duclings@users.noreply.github.com>
2024-01-09 10:02:18 -0500

    Create Random.java

---
+++ Random/Random.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Random
+{
+   public static void main(String[] args)
+   {
+      Integer[] numbers = {1,2,3,4,5,6,7,8,9};
+      
+      List<Integer> intList = Arrays.asList(numbers);
+      
+      Collections.shuffle(intList);
+      
+      intList.toArray(numbers);
+
+      for (int i = 0; i < 9; i++)
+      {
+         System.out.print(numbers[i]);
+      }
+   }
+}
